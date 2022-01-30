c6239395e7fee618271751907c30099c9525592e
Gregor <apriljunge@gmail.com>
2022-01-30 19:38:45 +0100

    Adds ptToCm filter and adapts units to tikz original

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("ptInCm", function(pt) {
+        // source https://www.overleaf.com/learn/latex/Lengths_in_LaTeX
+        return pt * 0.03515;
+    });
+
     return {
         dir: {
             input: "src",
