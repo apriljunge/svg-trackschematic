35c91e379b94303b4e214cceef5c758c9b94c456
Gregor <apriljunge@gmail.com>
2022-01-31 00:11:21 +0100

    Adds svgo for optimized svg output

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,33 @@
+const { optimize } = require('svgo');
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addTransform("svg-optimize", function(content) {
+        if( this.outputPath && this.outputPath.endsWith(".svg") ) {
+            const {data} = optimize(content, {
+                js2svg: {
+                    pretty: true
+                },
+                plugins: [
+                    {
+                        name: 'preset-default',
+                        params: {
+                            overrides: {
+                                minifyStyles: false,
+                                convertStyleToAttrs: false
+                            }
+                        }
+                    },
+                    'reusePaths',
+                    'sortAttrs'
+                ]
+            });
+
+            return data;
+        }
+    
+        return content;
+    });
+
     eleventyConfig.addFilter("ptInCm", function(pt) {
         // source https://www.overleaf.com/learn/latex/Lengths_in_LaTeX
         return pt * 0.03515;
