d5259e5d8010d2bc70fda044298182bca806b407
apriljunge <apriljunge@users.noreply.github.com>
2022-02-02 15:39:47 +0000

    deploy: 72be5273cbad30b8e869e81826218ff64bf5a6bf

--- static/js/docs.js
+++ static/js/docs.js
@@ -5,16 +5,12 @@ const formDataToElement = (form) => {
     targetElement.setAttribute('class', '');
 
     inputs.forEach((input) => {
-        switch (input.type) {
-            case "color":
-                targetElement.style.setProperty('--foreground', input.value);
-                break;
-            case "checkbox":
-            case "radio":
-                if (input.checked && !input.dataset.default && input.value) {
-                    targetElement.classList.add(input.value)
-                }
-                break;
+        if (input.dataset.property === undefined) {
+            if (input.checked && !input.dataset.default && input.value) {
+                targetElement.classList.add(input.value)
+            }
+        } else {
+            targetElement.style.setProperty(input.dataset.property, input.value);
         }
     });  
 } 
@@ -25,6 +21,10 @@ document.querySelectorAll('form[data-target]').forEach((form) => {
 
 document.querySelectorAll('input').forEach( input => {
     input.addEventListener('change', ({target}) => {
+        formDataToElement(target.closest('form'));
+    });
+
+    input.addEventListener('keyup', ({target}) => {
         formDataToElement(target.closest('form'));
     });
 });
